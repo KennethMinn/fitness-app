0b0641e3b1456fc0d27926b7a6d4a1c137e89129
KennethMinn <minthukha2472005@gmail.com>
2023-05-12 11:59:53 +0630

    implemented details page and related youtube vids

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -4,7 +4,7 @@ import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
-// import Loader from './Loader';
+import Loader from './Loader';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -37,7 +37,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     window.scrollTo({ top: 1800, behavior: 'smooth' });
   };
 
-  // if (!currentExercises.length) return <Loader />;
+  if (!currentExercises.length) return <Loader />;
 
   return (
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,15 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+  url: "https://youtube-search-and-download.p.rapidapi.com/video/related",
+  headers: {
+    "X-RapidAPI-Key": "40baddcaf3msh57c0ef4466c5b4dp103653jsn5c353c514173",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
